342ad3929b84815b4a2b2823289bbc40fc438c0d
Marcio Junior <marciojunior_eu@yahoo.com.br>
2024-10-31 13:07:39 -0300

    Move frontend to vite project

--- app/src/main/kotlin/org/example/BlogApplication.kt
+++ app/src/main/kotlin/org/example/BlogApplication.kt
@@ -8,6 +8,7 @@ import io.dropwizard.core.setup.Environment
 import io.dropwizard.jersey.validation.ConstraintMessage
 import io.dropwizard.jersey.validation.JerseyViolationException
 import io.dropwizard.util.Resources
+import jakarta.servlet.DispatcherType
 import jakarta.servlet.http.HttpServlet
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -15,9 +16,12 @@ import jakarta.validation.ConstraintViolation
 import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
 import jakarta.ws.rs.ext.ExceptionMapper
+import org.eclipse.jetty.servlets.CrossOriginFilter
 import org.example.resources.PostsResource
 import org.flywaydb.core.Flyway
 import org.jetbrains.exposed.sql.Database
+import java.util.*
+
 
 class CustomJerseyViolationExceptionMapper : ExceptionMapper<JerseyViolationException> {
     override fun toResponse(exception: JerseyViolationException): Response {
@@ -59,16 +63,37 @@ class BlogApplication : Application<BlogConfiguration>() {
     }
 
     override fun run(configuration: BlogConfiguration, environment: Environment) {
-        environment.servlets().addServlet("spa", ServeIndexServlet()).addMapping("/*")
+        setupCORS(environment)
+//        environment.servlets().addServlet("spa", ServeIndexServlet()).addMapping("/*")
         environment.jersey().register(PostsResource())
         environment.jersey().register(CustomJerseyViolationExceptionMapper())
+        setupDB(configuration, environment)
+    }
 
+    private fun setupDB(configuration: BlogConfiguration, environment: Environment) {
         val dataSource = configuration.database.build(environment.metrics(), "database")
         Database.connect(dataSource)
         environment.lifecycle().manage(dataSource)
 
         val flyway = Flyway.configure().dataSource(dataSource).load()
         flyway.migrate()
+    }
+
+    private fun setupCORS(environment: Environment) {
+        // Enable CORS headers
+        val cors = environment.servlets().addFilter("CORS", CrossOriginFilter::class.java)
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*")
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin")
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD")
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType::class.java), true, "/*")
+
+        // DO NOT pass a preflight request to down-stream auth filters
+        // unauthenticated preflight requests should be permitted by spec
+        cors.setInitParameter(CrossOriginFilter.CHAIN_PREFLIGHT_PARAM, "false")
     }
 
     companion object {
